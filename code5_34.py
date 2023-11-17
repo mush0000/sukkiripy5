6d4f63aac8e584a59bf1b47a091c686fb292fb7a
mush0000 <mashroom0825@gmail.com>
2023-11-17 11:41:44 +0900

    練習問題

--- code5_34.py
+++ code5_34.py
@@ -1,18 +1,28 @@
 #5-2
 
-year = int(input("うるう年を判定します（西暦を入力）>>"))
+# year = int(input("うるう年を判定します（西暦を入力）>>"))
 
-def leap_year(year):
-    if( year % 400 == 0 ):
-        print("西暦"+ str(year) + "は、うるう年です")
+# def leap_year(year):
+#     if( year % 400 == 0 ):
+#         print("西暦"+ str(year) + "は、うるう年です")
 
-    elif(year % 100 == 0 ):
-        print("西暦"+str(year) + "は、うるう年ではありません")     
+#     elif(year % 100 == 0 ):
+#         print("西暦"+str(year) + "は、うるう年ではありません")     
             
-    elif(year % 4 == 0):
-        print("西暦"+ str(year) + "は、うるう年です")
+#     elif(year % 4 == 0):
+#         print("西暦"+ str(year) + "は、うるう年です")
 
-    else:
-        print("西暦"+ str(year) + "は、うるう年ではありません")
+#     else:
+#         print("西暦"+ str(year) + "は、うるう年ではありません")
 
-leap_year(year)
+# leap_year(year)
+
+def is_leapyear(year):
+    return (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0))
+
+year = int(input("調べたい西暦を入力してください"))
+
+if(is_leapyear(year)):
+    input("{}はうるう年です".format(year))
+else:
+    input("{}はうるう年ではありません".format(year))
--- code5_35.py
+++ code5_35.py
@@ -1,10 +1,36 @@
 #5-5
 
+# #計算データの入力
+# def int_input(msg_amount,msg_people):
+#     amount = int(input(msg_amount))
+#     people = int(input(msg_people))
+#     return[amount,people]
+
+# #割り勘の計算
+# def calc_payment(amount,people=2):
+#     dnum = amount / people      #総額を人数で割る
+#     pay = dnum // 100 * 100     #100円未満を切り捨てる
+
+#     if dnum > pay:              #元の値と比較して、
+#         pay = int(pay + 100)    #小さければ100円未満があったので上乗せ
+    
+#     payorg = amount - pay * (people - 1)
+#     return[int(pay), int(payorg)]
+
+# #幹事の支払額の計算
+# def show_payment(pay,payorg,people=2):
+#     #結果の表示
+#     print("*** 支払額 ***")
+#     print("1人あたり{}円({}人)、幹事は{}円です".format(pay,people -1, payorg))
+
+# amount,people = int_input("支払総額を入力してください>>","参加人数を入力してください>>")
+# [pay,paylog] = calc_payment(amount,people)
+# show_payment(pay,paylog,people)
+
+
 #計算データの入力
-def int_input(msg_amount,msg_people):
-    amount = int(input(msg_amount))
-    people = int(input(msg_people))
-    return[amount,people]
+def int_input(msg):
+    return int(input("{}を入力してください>>".format(msg))) 
 
 #割り勘の計算
 def calc_payment(amount,people=2):
@@ -14,17 +40,20 @@ def calc_payment(amount,people=2):
     if dnum > pay:              #元の値と比較して、
         pay = int(pay + 100)    #小さければ100円未満があったので上乗せ
 
-    return[dnum,pay]
+    #幹事の支払額の計算
+    payorg = amount - pay * (people -1)
+    return (int(pay),int(payorg))
 
-#幹事の支払額の計算
-def show_payment(amount,pay,people=2):
-    payorg = amount - pay * (people - 1)
-
-    #結果の表示
+ #結果の表示
+def show_payment(pay,payorg,people=2):
     print("*** 支払額 ***")
-    print("1人あたり{}円({}人)、幹事は{}円です".format(pay,people -1, payorg))
+    print("1人あたり{}円({}人)、幹事は{}円です".format(pay,people -1,payorg))
 
-amount,people = int_input("支払総額を入力してください>>","参加人数を入力してください>>")
-dnum,pay = calc_payment(amount,people)
-show_payment(amount,pay,people)
+#関数の呼び出し
+amount = int_input("支払総額")
+people = int_input("参加人数")
+
+pay,payorg = calc_payment(amount,people)
+show_payment(pay,payorg,people)
+
 
